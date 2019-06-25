63af04e4676733c7ca9e16df9b96b18a21ba1e14
kamilp <kamil.powroznik@kissdigital.com>
2019-06-25 21:44:07 +0200

    Add animation event

--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import lottie from 'lottie-web';
 
 const $button = document.querySelector('.btn');
 const $modal = document.querySelector('.modal');
+const $modalTextField = document.querySelector('.modal__text-field');
 const $backgroundWrapper = document.querySelector('.background-wrapper');
 const $animationWrapper = document.querySelector('.animation-wrapper');
+
+let counter = 0;
 
 const animations = {
   boom: `https://assets10.lottiefiles.com/datafiles/U1I3rWEyksM9cCH/data.json`,
   waves: `https://assets2.lottiefiles.com/datafiles/N8DaINa2dmLOJja/data.json`
 };
-
-const boomAnimation = lottie.loadAnimation({
-  container: $animationWrapper,
-  renderer: 'svg',
-  loop: true,
-  autoplay: false,
-  path: animations.boom
-});
 
 lottie.loadAnimation({
   container: $backgroundWrapper,
@@ -26,6 +21,19 @@ lottie.loadAnimation({
   path: animations.waves
 });
 
+const boomAnimation = lottie.loadAnimation({
+  container: $animationWrapper,
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  path: animations.boom
+});
+
+boomAnimation.onLoopComplete = () => {
+  counter++;
+  $modalTextField.textContent = `Animation interations: ${counter}`;
+};
+
 $button.addEventListener('click', () => {
   $modal.classList.add('modal--is-open');
   boomAnimation.play();
@@ -33,4 +41,5 @@ $button.addEventListener('click', () => {
 
 $modal.addEventListener('click', () => {
   $modal.classList.remove('modal--is-open');
+  boomAnimation.stop();
 });
